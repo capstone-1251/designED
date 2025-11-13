571afeb8fcfdbe0dc10717cb56c6469176328978
whitherlaws <krispoebaikul@gmail.com>
2025-11-13 14:24:18 -0700

    Break normal css into scss and restructure the styling file

--- header.php
+++ header.php
@@ -20,14 +20,21 @@
                 <?php if (function_exists('the_custom_logo') && has_custom_logo()) : ?>
                     <div class="site-logo"><?php the_custom_logo(); ?></div>
                 <?php endif; ?>
-                <?php
-                wp_nav_menu(
-                    array(
-                        'theme_location' => 'top-menu',
-                        'menu_class' => 'nav-bar'
+                <nav>
+                    <?php
+                    wp_nav_menu(
+                        array(
+                            'theme_location' => 'top-menu',
+                            'menu_class' => 'nav-bar'
+                        )
                     )
-                )
-                ?>
+                    ?>
+                    <div class="search-btn">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="size-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                        </svg>
+                    </div>
+                </nav>
             </div>
         </div>
     </header>
