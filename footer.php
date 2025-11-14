779120cee4912139eedec9817fc714e7a01eb1e1
kheammarinb <156339004+kheammarinb@users.noreply.github.com>
2025-11-14 15:56:54 -0700

    updated footer

--- footer.php
+++ footer.php
@@ -6,6 +6,7 @@
         <div class="footer-container">
             <?php if (is_active_sidebar('main-sidebar')) : ?>
                 <aside class="sidebar">
+                    
                     <?php dynamic_sidebar('main-sidebar'); ?>
             
                 </aside>
