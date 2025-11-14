daff791ba270441a11d9543fc23cb05195732680
whitherlaws <krispoebaikul@gmail.com>
2025-11-14 13:39:29 -0700

    Edit GSAP enqueuing and make all the global fontsize for every part of the website

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <h1><?php the_title(); ?></h1>
 
-
+<h2>404 Page Not Found</h2>
 
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -38,7 +38,7 @@ function theme_gsap_script()
     // ScrollTrigger - with gsap.js passed as a dependency
     wp_enqueue_script('gsap-st', 'https://cdn.jsdelivr.net/npm/gsap@3.13.0/dist/ScrollTrigger.min.js', array('gsap-js'), false, true);
     // Your animation code file - with gsap.js passed as a dependency
-    wp_enqueue_script('gsap-js2', get_template_directory_uri() . 'js/app.js', array('gsap-js'), false, true);
+    wp_enqueue_script('gsap-js2', get_template_directory_uri() . '/js/app.js', array('gsap-js'), false, true);
 }
 
 add_action('wp_enqueue_scripts', 'theme_gsap_script');
