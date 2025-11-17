f5c84956be7cdbf21cc5cb23d0400272eaf4d97b
whitherlaws <krispoebaikul@gmail.com>
2025-11-17 16:09:37 -0700

    Finish Header

--- header.php
+++ header.php
@@ -33,6 +33,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="size-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                         </svg>
+                        <input type="text" id="search-btn" name="search-btn" placeholder="Search here" class="search-input">
                     </div>
                 </nav>
                 <div class="hamburger">
@@ -43,6 +44,21 @@
             </div>
         </div>
         <div class="hidden">
+            <div class="close">
+                <?php if (function_exists('the_custom_logo') && has_custom_logo()) : ?>
+                    <div class="site-logo"><?php the_custom_logo(); ?></div>
+                <?php endif; ?>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+            </div>
+            <div class="search">
+                Search
+                <input type="text" id="search" name="search" placeholder="Search here">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="black" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+                </svg>
+            </div>
             <?php
             wp_nav_menu(
                 array(
