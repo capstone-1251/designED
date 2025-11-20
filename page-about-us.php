bed4fab4e1fe444117963fe4983497c5c06c8db8
kheammarinb <156339004+kheammarinb@users.noreply.github.com>
2025-11-19 22:45:53 -0700

    update about us page

--- page-about-us.php
+++ page-about-us.php
@@ -10,48 +10,63 @@ $owner_picture = $owner_image['sizes']['custom-size'];
 ?>
 
 <?php get_header(); ?>
-<div>
+<div class="container">
     <div>
-        <h1><?php the_title(); ?></h1>
-        <ul>
-            <li><?php
-                the_field('about_us_1');
-                ?></li>
-            <li><?php
-                the_field('about_us_2');
-                ?></li>
-            <li><?php
-                the_field('about_us_3');
-                ?></li>
-        </ul>
-        <p>
-            <?php
-            the_field('about_us_description');
-            ?>
-        </p>
-        <p><?php
-            the_field('about_us_tagline');
-            ?></p>
+        
+        <div>
+            <h1><?php the_title(); ?></h1>
+            <div class="aboutus-underline"></div>
+            <div><img src="<?php echo $picture ?>" alt="<?php echo $about_image['alt']; ?>"></div>
+            <p>
+                <?php
+                the_field('about_us_description');
+                ?>
+            </p>
+            <ul>
+                <li><?php
+                    the_field('about_us_1');
+                    ?></li>
+                <li><?php
+                    the_field('about_us_2');
+                    ?></li>
+                <li><?php
+                    the_field('about_us_3');
+                    ?></li>
+            </ul>
+            <p><?php
+                the_field('about_us_tagline');
+                ?></p>
+                
+        </div>
+        
     </div>
-    <div><img src="<?php echo $picture ?>" alt="<?php echo $about_image['alt']; ?>"></div>
+    
+    <div class="owner-section">
+    <div class="owner-inner">
+    <div class="owner-text">
+      <h2><?php the_field('meet_the_owner'); ?></h2>
+      <div class="promo-underline"></div>
+
+      <p><?php the_field('owner_description'); ?></p>
+
+      <a href="/contact" class="owner-btn">Contact Us</a>
+    </div>
+
+    <div class="owner-media">
+      <div class="owner-photo-wrap">
+        <span class="owner-name">
+          <?php echo esc_html( $owner_image['title'] ?? 'Edward Gebran' ); ?>
+        </span>
+        <img
+          class="owner-photo"
+          src="<?php echo esc_url( $owner_picture ); ?>"
+          alt="<?php echo esc_attr( $owner_image['alt'] ); ?>"
+        >
+      </div>
+    </div>
+  </div>
 </div>
 
-<div>
-    <div>
-        <h2>
-            <?php
-            the_field('meet_the_owner');
-            ?>
-        </h2>
-        <p>
-            <?php
-            the_field('owner_description');
-            ?>
-        </p>
-    </div>
-    <div>
-        <div><img src="<?php echo $owner_picture ?>" alt="<?php echo $owner_image['alt']; ?>"></div>
-    </div>
 </div>
 
 
