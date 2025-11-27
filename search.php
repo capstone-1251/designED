f5be823257f93f9373e9ed680db484df473b691b
whitherlaws <krispoebaikul@gmail.com>
2025-11-27 13:06:32 -0700

    Working on the search page

--- search.php
+++ search.php
@@ -11,8 +11,20 @@
             <div class="search-result-item">
                 <a href="<?php the_permalink(); ?>">
                     <h3><?php the_title(); ?></h3>
+                    <?php
+                    if (has_post_thumbnail()):
+                    ?>
+                        <img src="<?php the_post_thumbnail_url('blog-xl') ?>" alt="<?php echo get_post_meta(get_post_thumbnail_id(), '_wp_attachment_image_alt', true) ?>" class="thumb">
+                    <?php
+                    endif
+                    ?>
                 </a>
-                <p><?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?></p>
+                <p>
+                    <?php
+                    the_field('exerpt');
+                    ?>
+                </p>
+                <a href="<?php echo esc_url(get_permalink()); ?>" class="btn">Explore <?php echo esc_html(get_the_title()); ?></a>
             </div>
         <?php
         endwhile;
